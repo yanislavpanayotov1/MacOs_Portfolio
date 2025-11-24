dd7b071507b3e61873bc894a6b3c25d2c5f00247
Yanislav Panayotov <panayotovyanislav@gmail.com>
2025-11-24 14:37:07 +0200

    first commit

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import dayjs from 'dayjs'
+import { navLinks } from '#constants/index.js'
+import { navIcons } from '../constants'
+
+
+const NavBar = () => {
+  return (
+    <nav>
+        <div> 
+            <img src="/images/logo.svg" alt="logo" />
+            <p className='font-bold'>Yanislav's Portfolio</p>
+
+            <ul>
+                {
+                    navLinks.map(({id, name}) => (
+                        <li key={id}>
+                            <p>
+                                {name}
+                            </p>                        
+                        </li>
+                    ))
+                }
+            </ul>
+        </div>
+        <div>
+            <ul>
+                {navIcons.map(({id, img}) => (
+                    <li key={id}>
+                        <img src={img} alt={`icon-${id}`} />
+                    </li>
+                ))}
+            </ul>
+            <time datetime="">{dayjs().format('ddd MMM D h:mm A')}</time>
+        </div>
+    </nav>
+  )
+}
+
+export default NavBar
